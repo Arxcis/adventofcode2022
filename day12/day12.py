ad0b81f202cdb72b92ea8bfff26c87dd2b94e399
Jonas Johan Solsvik <jonasjso@protonmail.com>
2022-12-13 00:22:24 +0100

    day12

--- day12/day12.py
+++ day12/day12.py
@@ -7,52 +7,108 @@ accszExk
 acctuvwj
 abdefghi
 """
+# Read input
 _map = [[cell for cell in row.strip()] for row in input()]
+
 # Add padding
-map = [["å" for _ in range(len(_map[0])+2)] for _ in range(len(_map)+2)]
+hightmap = [["å" for _ in range(len(_map[0])+2)] for _ in range(len(_map)+2)]
 for y, row in enumerate(_map):
     for x, cell in enumerate(row):
-        map[y+1][x+1] = cell
+        hightmap[y+1][x+1] = cell
 
-cost_map = [[-1 for _ in range(len(_map[0])+2)] for _ in range(len(_map)+2)]
 
+def find_steps_to_best_signal(start, hightmap):
+    # Simulate dijkstra-ish algorithm
+    seen = []
+    horizon = [start]
+    hightmap[start[1]][start[0]] = "a"
+    steps = 0
+
+    while True:
+        steps += 1
+        new_horizon = []
+        
+        def check_candidate(cx,cy,x,y):
+
+            if hightmap[y][x] == "z"\
+            and hightmap[cy][cx] == "E":
+                seen.append((x,y))
+                return True # Found the top with the best signal !!
+            
+            if (cx,cy) in seen:
+                return False 
+
+            if (cx,cy) in new_horizon:
+                return False
+
+            if ord(hightmap[cy][cx]) - ord(hightmap[y][x]) <= 1:
+                new_horizon.append((cx,cy))
+            
+            return False
+
+        if len(horizon) == 0:
+            return -1
+
+        for x,y in horizon:
+            found = check_candidate(x-1,y,x,y)
+            if found: break
+      
+            found = check_candidate(x+1,y,x,y)
+            if found: break
+                    
+            found = check_candidate(x,y-1,x,y)
+            if found: break
+            
+            found = check_candidate(x,y+1,x,y)
+            if found: break
+
+            seen.append((x,y))
+        
+        if found: break
+        
+        horizon = new_horizon
+
+    return steps
 
 #
 # ---------- PART 1 ----------
-running = True
-seen = []
-horizon = [(1,1)]
-map[1][1] = "a"
-steps = 0
+#
+start = (0,0)
+# Find start position
+for y, row in enumerate(hightmap):
+    for x, cell in enumerate(row):
+        if cell == "S":
+            start = (x,y)
+            break
+    if cell == "S":
+        break
 
-while running:
-    steps += 1
-    new_horizon = []
-    def check_candidate(cx,cy,x,y):
-        global steps 
+print(f"Start: ({start[0]}, {start[1]})")
 
-        if steps > 251:
-            print(steps, map[cy][cx])
-
-        if map[y][x] == "z"\
-        and map[cy][cx] == "E":
-            global running
-            running = False # end loop!!
-
-        elif (cx,cy) not in seen\
-        and (cx,cy) not in new_horizon\
-        and abs(ord(map[cy][cx]) - ord(map[y][x])) <= 1:
-            new_horizon.append((cx,cy))
-
-    for x,y in horizon:
-        check_candidate(x-1,y,x,y)
-        check_candidate(x+1,y,x,y)
-        check_candidate(x,y-1,x,y)
-        check_candidate(x,y+1,x,y)
-
-        seen.append((x,y))
-    
-    horizon = new_horizon
-
-
+steps = find_steps_to_best_signal(start, hightmap)
 print(steps)
+
+#
+# ------------- PART 2 ------------
+#
+start_positions = []
+for y, row in enumerate(hightmap):
+    for x, cell in enumerate(row):
+        if cell == "a":
+            start_positions.append((x,y))
+
+print(len(start_positions))
+
+results = []
+for start in start_positions:
+    steps = find_steps_to_best_signal(start, hightmap)
+    results.append(steps)
+    print(len(results))
+    
+results = list(filter(lambda a: a != -1, results))
+print(results)
+
+results.sort()
+print(results)
+
+
