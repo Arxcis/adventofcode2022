7a235b4776f8b625386033ccc2f730f24fc63897
Jonas Johan Solsvik <jonasjso@protonmail.com>
2022-12-12 11:01:30 +0100

    day12 draft

--- day12/day12.py
+++ day12/day12.py
@@ -21,15 +21,38 @@ cost_map = [[-1 for _ in range(len(_map[0])+2)] for _ in range(len(_map)+2)]
 # ---------- PART 1 ----------
 running = True
 seen = []
-cost_map[1,1] = 0
 horizon = [(1,1)]
+map[1][1] = "a"
+steps = 0
 
 while running:
+    steps += 1
+    new_horizon = []
+    def check_candidate(cx,cy,x,y):
+        global steps 
 
-    left_cost = map[y][x-1] - map[y][x-1]
-    right_cost = map[y][x+1]
-    up_cost = map[y-1][x]
-    down_cost = map[y+1][x]
+        if steps > 251:
+            print(steps, map[cy][cx])
 
+        if map[y][x] == "z"\
+        and map[cy][cx] == "E":
+            global running
+            running = False # end loop!!
 
+        elif (cx,cy) not in seen\
+        and (cx,cy) not in new_horizon\
+        and abs(ord(map[cy][cx]) - ord(map[y][x])) <= 1:
+            new_horizon.append((cx,cy))
+
+    for x,y in horizon:
+        check_candidate(x-1,y,x,y)
+        check_candidate(x+1,y,x,y)
+        check_candidate(x,y-1,x,y)
+        check_candidate(x,y+1,x,y)
+
+        seen.append((x,y))
     
+    horizon = new_horizon
+
+
+print(steps)
