d6facdfdaa8eeec3428dd751f938dff7d48c2d47
Jonas Johan Solsvik <jonasjso@protonmail.com>
2022-12-14 12:03:54 +0100

    day14 complete

--- day14/day14.py
+++ day14/day14.py
@@ -23,8 +23,8 @@ from functools import reduce
 rocks = [[(int(x),int(y)) for x,y in re.findall("(\d+),(\d+)", line)] for line in input()]
 flat_lines = reduce(lambda a,b: a+b, rocks)
 
-max_x = max(flat_lines, key=lambda it: it[0])[0] + 10 
-max_y = max(flat_lines, key=lambda it: it[1])[1] + 10
+max_x = max(flat_lines, key=lambda it: it[0])[0] + 120
+max_y = max(flat_lines, key=lambda it: it[1])[1] + 1
 
 START = "+"
 AIR = "."
@@ -36,78 +36,95 @@ sand_start = (500,0)
 grid = [[AIR for x in range(max_x)] for y in range(max_y)]
 grid[sand_start[1]][sand_start[0]] = START
 
-def print_grid():
-    for y in grid:
-        print("".join(y[450:]))
 
-for rock_line in rocks:
-    for a,b in zip(rock_line[0:-1], rock_line[1:]):
-        if a[1] == b[1]:
-            y = a[1]
-            if a[0] < b[0]:
-                for x in range(a[0], b[0]+1):
-                    grid[y][x] = ROCK
-            if a[0] > b[0]:
-                for x in range(a[0], b[0]-1, -1):
-                    grid[y][x] = ROCK
+def print_grid(grid):
+    for line in grid:
+        print("".join(line[400:]))
 
-        if a[0] == b[0]: 
-            x = a[0]
-            if a[1] < b[1]:
-                for y in range(a[1], b[1]+1):
-                    grid[y][x] = ROCK 
+
+def make_grid():
+    grid = [[AIR for x in range(max_x)] for y in range(max_y)]
+    grid[sand_start[1]][sand_start[0]] = START
+
+    for rock_line in rocks:
+        for a,b in zip(rock_line[0:-1], rock_line[1:]):
+            if a[1] == b[1]:
+                y = a[1]
+                if a[0] < b[0]:
+                    for x in range(a[0], b[0]+1):
+                        grid[y][x] = ROCK
+                else:
+                    for x in range(a[0], b[0]-1, -1):
+                        grid[y][x] = ROCK
+
+            elif a[0] == b[0]: 
+                x = a[0]
+                if a[1] < b[1]:
+                    for y in range(a[1], b[1]+1):
+                        grid[y][x] = ROCK 
+                else:
+                    for y in range(a[1], b[1]-1, -1):
+                        grid[y][x] = ROCK
             else:
-                for y in range(b[1], a[1]-1):
-                    grid[y][x] = ROCK 
+                print(a,b)
+                print("PANIC!!")
+                exit()
+    return grid
+
+def simulate_sand(grid):
+    i = 0
+
+    # Simulate sand
+    while True:
+        x,y = sand_start 
+        
+        # Simulate grain
+        while True:
+            # If below is air, sand falls 
+            if grid[y+1][x] == AIR or grid[y+1][x] == DUST:
+                y += 1
+                grid[y][x] = DUST
+            # else check down+left
+            elif grid[y+1][x-1] == AIR or grid[y+1][x-1] == DUST: 
+                y += 1
+                x -= 1
+                grid[y][x] = DUST
+            # else check down+right
+            elif grid[y+1][x+1] == AIR or grid[y+1][x+1] == DUST: 
+                y += 1
+                x += 1
+                grid[y][x] = DUST
+            # else sand is at rest :)
+            else:
+                i += 1
+                grid[y][x] = SAND
+                break
+        
+
+
+            # If sand is falling into the abyss...
+            if y == len(grid)-1: break
+        if y == len(grid)-1: break
+        
+        # If sand is blocking sand_start
+        if y == sand_start[1] and x == sand_start[0]: break
+    
+    return i
 
 #
 # -------------- PART 1 -------------
 #
-i = 0
-
-# Simulate sand
-while True:
-    x,y = sand_start 
-    
-    # Simulate grain
-    while True:
-        # If below is air, sand falls 
-        if grid[y+1][x] == AIR or grid[y+1][x] == DUST:
-            y += 1
-            grid[y][x] = DUST
-        # else check down+left
-        elif grid[y+1][x-1] == AIR or grid[y+1][x-1] == DUST: 
-            y += 1
-            x -= 1
-            grid[y][x] = DUST
-        # else check down+right
-        elif grid[y+1][x+1] == AIR or grid[y+1][x+1] == DUST: 
-            y += 1
-            x += 1
-            grid[y][x] = DUST
-        # else sand is at rest :)
-        else:
-            i += 1
-            grid[y][x] = SAND
-            break
-    
-        # If sand is falling into the abyss...
-        if y == len(grid)-1: break
-    if y == len(grid)-1: break
-    
-print(f"\n=== GAME OVER! === ")
-print_grid()
+grid = make_grid()
+i = simulate_sand(grid)
 print(i)
 
-sand_count = 0
-dust_count = 0
-rock_count = 0
-for row in grid:
-    for cell in row:
-        if cell == SAND: sand_count += 1
-        if cell == DUST: dust_count += 1
-        if cell == ROCK: rock_count += 1
+#
+# -------------- PART 2 -------------
+#
+grid = make_grid()
+grid.append([AIR for i in range(len(grid[0]))])
+grid.append([ROCK for i in range(len(grid[0]))])
 
-print(sand_count,dust_count,rock_count)
-
-
+i = simulate_sand(grid)
+print_grid(grid)
+print(i)
