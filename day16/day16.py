42ba34a7b9724527ae6fad3a0b4149973a831bb7
Jonas Johan Solsvik <jonasjso@protonmail.com>
2022-12-26 22:11:48 +0100

    day16 ongoing work.....

--- day16/day16.py
+++ day16/day16.py
@@ -32,36 +32,50 @@ for name, flow, tunnels in _valves:
 #
 # Precompute how far everything is from everything else
 #
-def count_minutes(target: str, valve: dict, seen: set, count: int):
+def count_minutes(target: str, parent: dict, valve: dict, seen: set, count: int):
     if target == valve["name"]:
         return count
     
-    if valve["name"] in seen:
-        return 999_999
-    seen.add(valve["name"])
-
     counts = []
     for tunnel in valve["tunnels"]:
-        counts.append(count_minutes(target, valves[tunnel], seen, count + 1))
+        if tunnel == parent["name"]:
+            continue
+        if (valve["name"], tunnel) in seen:
+            continue
+        counts.append(count_minutes(target, valve, valves[tunnel], set([(valve["name"], tunnel), *seen]), count + 1))
+
+    if len(counts) == 0:
+        return 999_999
 
     return min(counts)
 
+
+print(valves.values())
+
 for it in valves:
     for ot in valves:
-        minutes = count_minutes(ot, valves[it], set(), int())
+        minutes = count_minutes(ot, valves[it], valves[it], set(), int())
         valves[it]["minutes"][ot] = minutes
+
+        print(it, ot)
 
 #
 # Part 1
 #
 from itertools import permutations
+perm = valves.values()
+print(f"len(perm): {len(perm)}")
+
 perm = filter(lambda x: x["flow"] != 0, valves.values())
+print(f"len(perm): {len(perm)}")
+
 perm = map(lambda x: x["name"], perm)
+print(f"len(perm): {len(perm)}")
+
 perm = permutations(perm)
+print(f"len(perm): {len(perm)}")
 
-max_pressure = 0
-
-for i, keys in enumerate(perm):
+def compute_pressure(keys, valves):
     current = "AA"
     minute = 1
     pressure = 0
@@ -73,27 +87,42 @@ for i, keys in enumerate(perm):
 
     for next in keys:
         print(f"At {current}, moving to {next}")
+        print(f"Total {total}")
 
-        # Step 1: Move to new node, taking X minutes
+        # Step 1: Move to new node, and open it, taking X minutes
         minute += valves[current]["minutes"][next]
         total += pressure * valves[current]["minutes"][next]
         print(f"\n== MINUTE {minute} ==")
         print(f"Releasing {pressure} pressure")
         print(f"At {next}, opening valve with {valves[next]['flow']} pressure")
+        print(f"Total {total}")
+        pressure += valves[next]["flow"]
 
-        # Step 2: Open valve at the new node, increasing pressure, taking 1 minute
+        # Step 2: Wait one minute, increasing pressure, taking 1 minute
         minute += 1
         total += pressure * 1
-        pressure += valves[next]["flow"]
         print(f"\n== MINUTE {minute} ==")
-        print(f"Releasing {pressure}")
+        print(f"Releasing {pressure} pressure")
     
         # Step 3: Make next the new current
         current = next
 
-        
-    if total > max_pressure:
-        max_pressure = total
+    print(f"Total {total}")
 
-    break
+    for minute in range(minute+1, 31):
+        total += pressure * 1
+        print(f"\n== MINUTE {minute} ==")
+        print(f"Releasing {pressure} pressure")
+        print(f"Total {total}")
+
+max_pressure = 0
+pressure = compute_pressure(("DD", "BB", "JJ", "HH", "EE", "CC"), valves)
+print(pressure)
+exit(0)
+
+for i, keys in enumerate(perm):
+    pressure = compute_pressure(keys, valves)
+    if pressure > max_pressure:
+        max_pressure = pressure
+
 print(f"max_pressure: {max_pressure}")
